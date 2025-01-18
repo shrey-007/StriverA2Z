3772c325ee047d00de248654eff71b64780c9209
shreySaxena <shreyyerhs07@gmail.com>
2025-01-18 14:42:17 +0530

    lis

---
+++ src/DP/LIS/LongestBitonicSubsequence.java
@@ -0,0 +1,16 @@
+package DP.LIS;
+
+public class LongestBitonicSubsequence {
+    /**
+     * Given an array of positive integers. Find the maximum length of Bitonic subsequence.
+     * A subsequence of array is called Bitonic if it is first strictly increasing, then strictly decreasing.
+     * Return the maximum length of bitonic subsequence.
+     *
+     * Note : A strictly increasing or a strictly decreasing sequence should not be considered as a bitonic sequence
+     * */
+
+    public static int LongestBitonicSequence(int n, int[] nums) {
+        // code here
+        return 0;
+    }
+}
