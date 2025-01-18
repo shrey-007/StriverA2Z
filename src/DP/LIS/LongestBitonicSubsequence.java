2ae7f33f7d6e3c1f2db77fedfc1af1246c38d5fa
shreySaxena <shreyyerhs07@gmail.com>
2025-01-18 15:01:51 +0530

    longest bitonic sequence

--- src/DP/LIS/LongestBitonicSubsequence.java
+++ src/DP/LIS/LongestBitonicSubsequence.java
@@ -9,8 +9,47 @@ public class LongestBitonicSubsequence {
      * Note : A strictly increasing or a strictly decreasing sequence should not be considered as a bitonic sequence
      * */
 
+    // the code requires understanding of dp[] array int the tabulation code
     public static int LongestBitonicSequence(int n, int[] nums) {
-        // code here
-        return 0;
+        // Arrays to store LIS and LDS
+        int[] lis = new int[n];
+        int[] lds = new int[n];
+
+        // Initialize LIS for each element as 1
+        for (int i = 0; i < n; i++) {
+            lis[i] = 1;
+        }
+
+        // Compute LIS for every index
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+        }
+
+        // Initialize LDS for each element as 1
+        for (int i = 0; i < n; i++) {
+            lds[i] = 1;
+        }
+
+        // Compute LDS for every index
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (nums[i] > nums[j] && lds[i] < lds[j] + 1) {
+                    lds[i] = lds[j] + 1;
+                }
+            }
+        }
+
+        // Find the maximum length of bitonic subsequence
+        int maxBitonicLength = 0;
+        for (int i = 0; i < n; i++) {
+            maxBitonicLength = Math.max(maxBitonicLength, lis[i] + lds[i] - 1);
+        }
+
+        return maxBitonicLength;
     }
+
 }
