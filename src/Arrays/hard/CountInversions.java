2e8414f058e7b9bd04a34b611093c9673d9b61af
shreySaxena <shreyyerhs07@gmail.com>
2025-08-14 21:20:37 +0530

    count inversions

--- src/Arrays/hard/CountInversions.java
+++ src/Arrays/hard/CountInversions.java
@@ -5,6 +5,38 @@ import java.util.ArrayList;
 public class CountInversions {
     // method 1 is simple 2 for loops
 
+    // method 2 mai ese socho ki 2 sorted arrays hai
+    // [2,3,5,6] and [2,2,4,4,8]
+    // and i tumhe 1st array se lena hai and j tumhe 2nd array se lena hai
+    /**
+     * [2,3,5,6]   [2,2,4,4,8]
+     *  i           j  toh yaha arr[i]>arr[j] nahi hai toh socho kaha jaoge, tumhe chaiye ki left array vala bada ho
+     *  right array vale se, toh tum i++ karoge na.
+     *
+     *  [2,3,5,6]  [2,2,4,4,8]
+     *     i        j         abhi arr[i]>arr[j] hai toh left array mai i ke baad jitne bhi elements hai voh sab bhi
+     *  arr[j] se bade hai, toh total pairs 3. {3,2}{5,2}{6,2}. Toh is j ko bada do ab
+     *
+     *  [2,3,5,6]  [2,2,4,4,8]
+     *     i          j         abhi arr[i]>arr[j] hai toh left array mai i ke baad jitne bhi elements hai voh sab bhi
+     *  arr[j] se bade hai, toh total pairs 3. {3,2}{5,2}{6,2}. Toh is j ko bada do ab
+     *
+     *  [2,3,5,6]  [2,2,4,4,8]
+     *     i            j         abhi arr[i]>arr[j] nahi hai toh i++ kro
+     *
+     *  [2,3,5,6]  [2,2,4,4,8] abhi arr[i]>arr[j] hai toh left array mai i ke baad jitne bhi elements hai voh sab bhi
+     *       i          j
+     *  arr[j] se bade hai, toh total pairs 2. {5,4}{6,4}. Toh is j ko bada do ab.
+     *
+     *  and so on. Toh ye question agar esa hota 2 sorted vala toh easy hota, toh ise esa bana do, isme merge sirt lagao
+     *  merge function mai tumhe 2 sorted arrays milege unse coun nikaal lena inverse pairs ka, and unko merge bhi kr dena
+     *
+     *  means count bhi krte jaaoa and sort bhi krte jaao
+     *
+     * */
+
+
+
     // method 2 we use mergeSort, watch striver video get how its working or if you can dry run mergeSort it will easy for you
 
     private static int merge(int[] arr, int low, int mid, int high) {
