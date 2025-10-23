cc1c42a7fe7aae31c96879b887061268cc24977b
shreySaxena <shreyyerhs07@gmail.com>
2025-10-23 18:34:58 +0530

    paranthesis

--- src/Greedy/easy/ValidParenthesisString.java
+++ src/Greedy/easy/ValidParenthesisString.java
@@ -14,7 +14,7 @@ import java.util.Stack;
  * */
 public class ValidParenthesisString {
 
-    // Right now forget about *, just think how can we find whether parenthesis are balanced or not
+    // Right now forget about *, just think how can we find whether parenthesis are balanced or not, when we only have ( and )
     // earlier we used to check using stack, but we can do this using this way also-:
 
     public boolean func(String s){
@@ -34,57 +34,52 @@ public class ValidParenthesisString {
         return count==0;
     }
 
-    // Now come to question
-    // Brute force could be ,generate all possible combinations through recursion where * can take any values.
-    // Then check each combination
-    // Let's first do it using recursion
+    // so now coming to question
+    // take 2 pointers maxCount and minCount
+    // maxCount = consider all * as (
+    // minCount = consider all * as )
+    // now when a ( and ) comes, you need to treat maxCount and minCount as count only as you do above
+    // uper vale question mai count ek fixed value hai , but yaha it is not fixed, yaha range hogi count ki
 
-    public boolean main(String s) {
-        return recursive(s,0,0);
-    }
-    public boolean recursive(String s,int index,int count){
-        if(count<0){return false;}
-        if(index==s.length()){
-            if(count==0){return true;}
-            else{return false;}
-        }
-
-        char curr = s.charAt(index);
-        // opening bracket mai count badega
-        if(curr=='('){return recursive(s,index+1,count+1);}
-
-        // closing bracket mai count ghatega
-        else if(curr==')'){return recursive(s,index+1,count-1);}
-
-        else{
-            // means it is * toh 3 option hai ya toh ise opening maano jisme count badega ya ise closing maano jisme
-            // count ghatega ya empty maano jisme count same rahega
-            boolean faith1 = recursive(s,index+1,count+1);
-            boolean faith2 = recursive(s,index+1,count-1);
-            boolean faith3 = recursive(s,index+1,count);
-            return faith3 || faith2 || faith1;
-        }
-    }
-
-    // Here time and space complexity will be high so apply memoization, tabulation etc
     public boolean checkValidString(String s) {
-        int min = 0;
-        int max = 0;
+        int minCount = 0;
+        int maxCount = 0;
         int n = s.length();
 
         for(int i=0;i<n;i++){
             char curr = s.charAt(i);
-            if(curr=='('){min++;max++;}
-            else if(curr==')'){min--;max--;}
+            // since it is opening bracket, consider both min and maxCount as count jaise uper vale question mai kra hai
+            // toh ( aane pr count badate hai toh dono ko bada do
+            if(curr=='('){minCount++;maxCount++;}
+            // since it is closing bracket, consider both min and maxCount as count jaise uper vale question mai kra hai
+            // toh ) aane pr count kam krte hai toh dono ko kam kr do
+            else if(curr==')'){minCount--;maxCount--;}
+            // now comes *
+            // consider * as (, toh ( aane par count badate hai toh maxCount++
+            // consider * as ), toh ) aane par count ghatate hai toh minCount--
             else{
-                min--;
-                max++;
+                minCount--;
+                maxCount++;
             }
-            if(min<0){min=0;}
-            if(max<0){return false;}
+            // if maxCount<0 means ki agar maine saare * ko ( maan liya tab bhi string balanced nhi hai, there are many
+            // ), so return false
+            if(maxCount<0){return false;}
+            // Ab yaha tak aaye means maxCount>=0. Toh abhi maxCount>=0 and minCount<0, means ki itne * hai ki ki ( balanced
+            // hai, but ) abhi balanced nhi hai, toh maybe voh baad mai balanced ho jaaye.
+            // Just remember ye bas ek range hai count ki count minCount to maxCount jaa skta hai, but count ko negative
+            // nhi hona chaiye , count ki range negative jaa hi nhi nahi skti, agar gayi toh voh valid nahi hai, toh 0 krdo
+            // lets take example "(**)", 3rd index par jab aaoge toh minCount<0 ho jaaega, but this string is balanced toh minCount<0 is not a problem
+            if(minCount<0){minCount=0;}
         }
 
-        return min==0;
+
+        // count lies in range [minCount,maxCount]. Now to make a string balanced we check at the end ki count=0 or not
+        // yaha bhi vahi karo count, so 0 must lie in range [minCount,maxCount], since minCount is never negative
+        // toh minCoun itself starts from either 0 or some positive number. so just check if minCount=0 or not
+        // if minCount is not 0, means it is >0 toh count kabhi 0 ho hi nahi paaega toh it is not a valid one
+        return minCount==0;
+        // in simple words maxCount negative nhi hona chaiye and minCount 0 hona chaiye at the end. Jyaada vala kam nhi
+        // hona chaiye and kam vala jyaada nhi hona chaiye
     }
 
 }
